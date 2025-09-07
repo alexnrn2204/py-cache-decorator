9a4569137067615e4e496c737c2b56c857768ba4
alexnrn2204 <alexnrn2204@gmail.com>
2025-09-07 08:01:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_stores = []
+
+    def wrapper(*args, **kwargs) -> Callable:
+        cache_find = [
+            cache_store
+            for cache_store in cache_stores
+            if cache_store["args"] == args and cache_store["kwargs"] == kwargs
+        ]
+        if len(cache_find) == 0:
+            result = func(*args, **kwargs)
+            cache_stores.append({
+                "args": args,
+                "kwargs": kwargs,
+                "result": result
+            })
+            print("Calculating new result")
+        else:
+            result = cache_find[0]["result"]
+            print("Getting from cache")
+
+        return result
+
+    return wrapper
